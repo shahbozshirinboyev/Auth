ee228878ad91207358cded2cfde0071760d1f290
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-12-04 11:39:06 +0500

    new future logout button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
-function HomePage({ token }) {
+import { useNavigate } from "react-router-dom";
+
+function HomePage({ token, setToken }) {
+    let navigate = useNavigate()
+    const handleLogout =  () => {
+        sessionStorage.removeItem('token')
+        setToken(false)
+        navigate('/')
+    }
   return (
-    <div>
-      <p className="text-center py-4">Welcome back, {token.user.user_metadata.full_name}</p>
+    <div className="container grid grid-cols-2 gap-4 py-12">
+      <div className="flex justify-start">
+        <p>
+          Welcome back, {token.user.user_metadata.full_name}
+        </p>
+      </div>
+      <div className="flex justify-end">
+        <button onClick={handleLogout} className="btn btn-sm w-[70px]">Logout</button>
+      </div>
     </div>
   );
 }
